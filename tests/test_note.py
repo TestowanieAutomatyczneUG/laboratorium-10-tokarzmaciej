37f3b0bcd17bb76fcaf13733346dcefad35545ce
Maciej <maciej.tokarz1@wp.pl>
2020-12-07 19:10:59 +0100

    test_check_note_positive

--- tests/test_note.py
+++ tests/test_note.py
@@ -5,3 +5,6 @@ from src.sample.note import Note
 class TestNote(unittest.TestCase):
     def test_check_name_positive(self):
         self.assertEqual(Note("myNote", 5.5).get_name(), "myNote")
+
+    def test_check_note_positive(self):
+        self.assertEqual(Note("testNote", 2.5).get_note(), 2.5)
